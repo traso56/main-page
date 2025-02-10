8db2df2b7adab67b8a914b52689e342688bd2a1a
Muhammad Fadhlan <muhammadfadhlan4@gmail.com>
2025-02-10 09:36:36 +0700

    added hmr

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -1,7 +1,7 @@
-// import { hmrPlugin, presets } from '@open-wc/dev-server-hmr';
+import { hmrPlugin, presets } from '@open-wc/dev-server-hmr';
 
 /** Use Hot Module replacement by adding --hmr to the start command */
-const hmr = process.argv.includes('--hmr');
+const hmr = true
 
 export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   open: '/',
@@ -10,7 +10,7 @@ export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   nodeResolve: {
     exportConditions: ['browser', 'development'],
   },
-  
+
   /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
   // esbuildTarget: 'auto'
 
@@ -19,7 +19,7 @@ export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
 
   plugins: [
     /** Use Hot Module Replacement by uncommenting. Requires @open-wc/dev-server-hmr plugin */
-    // hmr && hmrPlugin({ exclude: ['**/*/node_modules/**/*'], presets: [presets.litElement] }),
+    hmr && hmrPlugin({ exclude: ['**/*/node_modules/**/*'], presets: [presets.lit], }),
   ],
 
   // See documentation for all available options
